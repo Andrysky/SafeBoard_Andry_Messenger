546978f5f906a2ee92c882cbb3ab53bfa7935cd5
andry <airmanhip@gmail.com>
2016-12-06 15:20:21 +0300

    Add project files.

---
+++ WpfApplication1/Interop/ManagerDll.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+
+namespace WpfApplication1.Interop
+{
+	public class ManagerDll
+	{
+		[DllImport(TodoManagerLib)]
+		public static extern bool Connect();
+
+		[DllImport(TodoManagerLib)]
+		public static extern void Close();
+
+		[DllImport(TodoManagerLib)]
+		public static extern int get();
+
+		[DllImport(TodoManagerLib, CallingConvention = CallingConvention.Cdecl)]
+		public static extern int plus(int a);
+
+		//[DllImport(TodoManagerLib, CallingConvention = CallingConvention.Cdecl)]
+		//public static extern void CreateItem(ref TodoItem todoItem);
+
+		//[DllImport(TodoManagerLib, CallingConvention = CallingConvention.Cdecl)]
+		//public static extern int GetItems([MarshalAs(UnmanagedType.LPArray)][Out] TodoItem[] todoItems, long itemsLength);
+
+		private const string TodoManagerLib = "C:/Users/Andry/Source/Repos/SafeBoardMessenger/Components/messenger/test1/Debugdll.dll";
+	}
+}
---
+++ dll/dllmain.cpp
@@ -0,0 +1,58 @@
+// dllmain.cpp : Defines the entry point for the DLL application.
+//#include "todo_manager.h"
+
+//static todo_sample::TodoManager g_todoManager;
+
+extern "C"
+{
+	int temp_test;
+	bool __declspec(dllexport) Connect()
+	{
+		temp_test = 0;
+		return true;// g_todoManager.Connect();
+	}
+
+	void __declspec(dllexport) Close()
+	{
+		temp_test = 0;
+		//g_todoManager.Close();
+	}
+
+	void __declspec(dllexport) inc()
+	{
+		temp_test++;
+	}
+
+	void __declspec(dllexport) __cdecl plus(int a)
+	{
+		temp_test+=a;
+	}
+
+	int __declspec(dllexport) get()
+	{
+		return temp_test;
+	}
+
+
+
+
+	/*
+	void __declspec(dllexport) __cdecl CreateItem(todo_sample::TodoItem* item)
+	{
+		*item = g_todoManager.CreateItem();
+	}
+
+	int __declspec(dllexport) __cdecl GetItems(todo_sample::TodoItem* items, int maxItemsLength)
+	{
+		auto itemsVector = g_todoManager.GetItems();
+
+		if (itemsVector.size() <= static_cast<size_t>(maxItemsLength))
+		{
+			memset(items, 0, sizeof(todo_sample::TodoItem) * maxItemsLength);
+			memcpy(items, &itemsVector[0], sizeof(todo_sample::TodoItem) * itemsVector.size());
+		}
+
+		return itemsVector.size();
+	}
+	*/
+}
